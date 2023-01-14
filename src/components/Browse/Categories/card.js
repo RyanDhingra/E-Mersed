9c76fb75237d5029f43803271a033b82b424f8b6
Ryan Dhingra <ryandhingra@gmail.com>
2023-01-13 19:35:10 -0500

    Hiding overflow for all pages except product browse pages.

--- src/components/Browse/Categories/card.js
+++ src/components/Browse/Categories/card.js
@@ -17,6 +17,7 @@ function Card({ prods, addToCart }) {
         <section className="neon bd-container">
             <div className="neon__container">
                 {items.map((prod, index) => (
+                    <>
                     <div key={index} className="neon__card">
                         <img style={{width: '200px', height: '100px'}} src={prod.image.url} alt='Keyboard'/>
                         <h1 className="neon__title">{prod.name}</h1>
@@ -25,6 +26,71 @@ function Card({ prods, addToCart }) {
                             Add to cart
                         </h3>
                     </div>
+                    <div key={index} className="neon__card">
+                        <img style={{width: '200px', height: '100px'}} src={prod.image.url} alt='Keyboard'/>
+                        <h1 className="neon__title">{prod.name}</h1>
+                        <p className="neon__description" dangerouslySetInnerHTML={{__html: prod.description}}></p>
+                        <h3 style={{textDecoration: 'none'}} onClick={() => addItem(prod)} className="neon__button">
+                            Add to cart
+                        </h3>
+                    </div>
+                    <div key={index} className="neon__card">
+                        <img style={{width: '200px', height: '100px'}} src={prod.image.url} alt='Keyboard'/>
+                        <h1 className="neon__title">{prod.name}</h1>
+                        <p className="neon__description" dangerouslySetInnerHTML={{__html: prod.description}}></p>
+                        <h3 style={{textDecoration: 'none'}} onClick={() => addItem(prod)} className="neon__button">
+                            Add to cart
+                        </h3>
+                    </div>
+                    <div key={index} className="neon__card">
+                        <img style={{width: '200px', height: '100px'}} src={prod.image.url} alt='Keyboard'/>
+                        <h1 className="neon__title">{prod.name}</h1>
+                        <p className="neon__description" dangerouslySetInnerHTML={{__html: prod.description}}></p>
+                        <h3 style={{textDecoration: 'none'}} onClick={() => addItem(prod)} className="neon__button">
+                            Add to cart
+                        </h3>
+                    </div>
+                    <div key={index} className="neon__card">
+                        <img style={{width: '200px', height: '100px'}} src={prod.image.url} alt='Keyboard'/>
+                        <h1 className="neon__title">{prod.name}</h1>
+                        <p className="neon__description" dangerouslySetInnerHTML={{__html: prod.description}}></p>
+                        <h3 style={{textDecoration: 'none'}} onClick={() => addItem(prod)} className="neon__button">
+                            Add to cart
+                        </h3>
+                    </div>
+                    <div key={index} className="neon__card">
+                        <img style={{width: '200px', height: '100px'}} src={prod.image.url} alt='Keyboard'/>
+                        <h1 className="neon__title">{prod.name}</h1>
+                        <p className="neon__description" dangerouslySetInnerHTML={{__html: prod.description}}></p>
+                        <h3 style={{textDecoration: 'none'}} onClick={() => addItem(prod)} className="neon__button">
+                            Add to cart
+                        </h3>
+                    </div>
+                    <div key={index} className="neon__card">
+                        <img style={{width: '200px', height: '100px'}} src={prod.image.url} alt='Keyboard'/>
+                        <h1 className="neon__title">{prod.name}</h1>
+                        <p className="neon__description" dangerouslySetInnerHTML={{__html: prod.description}}></p>
+                        <h3 style={{textDecoration: 'none'}} onClick={() => addItem(prod)} className="neon__button">
+                            Add to cart
+                        </h3>
+                    </div>
+                    <div key={index} className="neon__card">
+                        <img style={{width: '200px', height: '100px'}} src={prod.image.url} alt='Keyboard'/>
+                        <h1 className="neon__title">{prod.name}</h1>
+                        <p className="neon__description" dangerouslySetInnerHTML={{__html: prod.description}}></p>
+                        <h3 style={{textDecoration: 'none'}} onClick={() => addItem(prod)} className="neon__button">
+                            Add to cart
+                        </h3>
+                    </div>
+                    <div key={index} className="neon__card">
+                        <img style={{width: '200px', height: '100px'}} src={prod.image.url} alt='Keyboard'/>
+                        <h1 className="neon__title">{prod.name}</h1>
+                        <p className="neon__description" dangerouslySetInnerHTML={{__html: prod.description}}></p>
+                        <h3 style={{textDecoration: 'none'}} onClick={() => addItem(prod)} className="neon__button">
+                            Add to cart
+                        </h3>
+                    </div>
+                    </>
                 ))}
             </div>
         </section>
