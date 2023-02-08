c7bf388274229ea7cde6e543e2532f5aa722c013
Ryan Dhingra <ryandhingra@gmail.com>
2023-02-08 13:57:06 -0500

    Prod Testing

--- src/components/VSD/scene.js
+++ src/components/VSD/scene.js
@@ -18,7 +18,14 @@ const Scene = ({ setSelected, enableControls }) => {
         <ambientLight intensity={2} />
         <spotLight intensity={1} position={[0, 50, 0]} angle={1} penumbra={0.1} />
         <pointLight intensity={1} position={[0, -10, 0]} />
-        
+        <Table/>
+
+        <MouseModel isSelected={setSelected}/>
+        <MonitorModel isSelected={setSelected}/>
+        <KeyboardModel isSelected={setSelected}/>
+        <MicModel isSelected={setSelected}/>
+        <HeadphoneModel isSelected={setSelected}/>
+        <PCModel isSelected={setSelected}/>
         <ChairModel isSelected={setSelected}/>
         <PerspectiveCamera makeDefault position={[-4.5, 2.3, 4]} rotation={[0, Math.PI/4, 0]} fov={50} />
         <OrbitControls enabled={enableControls}/>
@@ -30,10 +37,3 @@ const Scene = ({ setSelected, enableControls }) => {
 export default Scene;
 
 //<primitive object={new THREE.AxesHelper(10)} />
-/*<Table />
-<MouseModel isSelected={setSelected}/>
-<MonitorModel isSelected={setSelected}/>
-<KeyboardModel isSelected={setSelected}/>
-<MicModel isSelected={setSelected}/>
-<HeadphoneModel isSelected={setSelected}/>
-<PCModel isSelected={setSelected}/>*/
